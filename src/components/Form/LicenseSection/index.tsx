8014e920a40fa071c387c2a2e2916ed1437a2aad
1ike <googlohren@gmail.com>
2022-07-03 18:12:54 +0300

    Remove some duplication in styles.

---
+++ src/components/Form/LicenseSection/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from './Section.module.scss';
+
+
+interface Props {
+  children: React.ReactNode,
+  headerText?: string,
+  containerClassName?: string,
+}
+
+function LicenseSection({ headerText, children, containerClassName }: Props) {
+  return (
+    <div className={containerClassName}>
+      {headerText && <h2 className={styles.header}>{headerText}</h2>}
+      <div className={styles.rows}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default LicenseSection;
